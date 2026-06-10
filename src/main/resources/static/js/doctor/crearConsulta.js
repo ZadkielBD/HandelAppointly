4b50da35b983b11b469b381baa6206c5e8d5e85a
Zadkiel <zadkielbd@outlook.com>
2026-06-10 00:49:50 -0600

    Handel Appointly v1.0

---
+++ src/main/resources/static/js/doctor/crearConsulta.js
@@ -0,0 +1,12 @@
+initSearchSelect({
+    wrapperId: 'medicinaWrap',
+    dataId: 'medicinaData',
+    inputId: 'medicinaInput',
+    inputRowId: 'medicinaInputRow',
+    dropdownId: 'medicinaDropdown',
+    hiddenFieldsId: 'medicinaHiddenFields',
+    fieldName: 'medicinaId',
+    badgeText: 'Medicina',
+    icon: '💊',
+    multiple: false
+});
---
+++ src/main/resources/static/js/pago/pagar.js
@@ -0,0 +1,25 @@
+initSearchSelect({
+    wrapperId: 'divisaWrap',
+    dataId: 'divisaData',
+    inputId: 'divisaInput',
+    inputRowId: 'divisaInputRow',
+    dropdownId: 'divisaDropdown',
+    hiddenFieldsId: 'divisaHiddenFields',
+    fieldName: 'codigoDivisa',
+    badgeText: 'Divisa',
+    icon: '💱',
+    multiple: false
+});
+
+initSearchSelect({
+    wrapperId: 'metodoPagoWrap',
+    dataId: 'metodoPagoData',
+    inputId: 'metodoPagoInput',
+    inputRowId: 'metodoPagoInputRow',
+    dropdownId: 'metodoPagoDropdown',
+    hiddenFieldsId: 'metodoPagoHiddenFields',
+    fieldName: 'metodoPago',
+    badgeText: 'Método',
+    icon: '💳',
+    multiple: false
+});
